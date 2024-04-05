b9cebbf064bf0dde3b6bb6e9ef524aba0949a684
DARMAGRANDPRIX <163404531+DARMAGRANDPRIX@users.noreply.github.com>
2024-04-05 11:55:49 +0900

    Update event.js

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,5 +1,5 @@
 
-$(window).on('click', function () {
+$(window).on('load', function () {
     console.log('loadイベントが発生しました')
 }), $(window).on('scroll', function () {
     console.log('scrollイベントが発生しました')
